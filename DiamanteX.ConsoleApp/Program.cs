bf3c5f70de8b0c52c8bc73482c5ccb4eb14a5300
Leonardo Dallago de Cordova <leodallago@outlook.com>
2024-03-29 09:21:18 -0300

    Diamente de letras

--- DiamanteX.ConsoleApp/Program.cs
+++ DiamanteX.ConsoleApp/Program.cs
@@ -2,64 +2,66 @@
 {
     internal class Program
     {
+        static string alphabeto = "abcdefghijklmnopqrstuvwxyz";
+
+        static char[] alphabetoArray = alphabeto.ToCharArray();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Diamante X");
 
-            Console.WriteLine("Por favor, Informe um numero");
-            int tamanhoDiamante = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Por favor, informe o estilo do diamante");
-            string estilo = Console.ReadLine();
+            Console.WriteLine("Por favor, Informe um numero");
+            char tamanhoDiamante = Convert.ToChar(Console.ReadLine());
+
+            int tamanhoNumerado = Array.IndexOf(alphabetoArray, tamanhoDiamante);
+            /*  Console.WriteLine("Por favor, informe o estilo do diamante");
+             string estilo = Console.ReadLine(); */
 
             int quantidadeDeX = 1;
             int contador = 0;
 
-            if (tamanhoDiamante % 2 == 0)
+
+            for (int linha = 0; linha < tamanhoNumerado; linha++)
             {
-                Console.WriteLine("Numero invalido, Por favor informe um numero IMPAR");
+                FazLinha(tamanhoNumerado, quantidadeDeX, contador);
+                contador++;
+                quantidadeDeX += 2;
+
+                Console.WriteLine();
+
             }
 
-            else
+            for (int linha = 0; linha <= tamanhoNumerado - 1; linha++)
             {
+                FazLinha(tamanhoNumerado, quantidadeDeX, contador);
+                contador--;
+                quantidadeDeX -= 2;
 
-                for (int linha = 0; linha < (tamanhoDiamante - 1) / 2; linha++)
-                {
-                    FazLinha(tamanhoDiamante, quantidadeDeX, contador,estilo);
-                    contador++;
-                    quantidadeDeX += 2;
-
-                    Console.WriteLine();
-
-                }
-
-                for (int linha = 0; linha <= (tamanhoDiamante - 1) / 2; linha++)
-                {
-                    FazLinha(tamanhoDiamante, quantidadeDeX, contador,estilo);
-                    contador--;
-                    quantidadeDeX -= 2;
-
-                    Console.WriteLine();
-                }
-
-
+                Console.WriteLine();
             }
 
             Console.ReadLine();
+
         }
 
-        private static void FazLinha(int tamanhoDiamante, int quantidadeDeX, int contador,string estilo)
+
+
+        private static void FazLinha(int tamanhoNumerado, int quantidadeDeX, int contador)
         {
-            for (int espaco = contador; espaco < (tamanhoDiamante - 1) / 2; espaco++)
+            for (int espaco = contador; espaco < tamanhoNumerado; espaco++)
             {
                 Console.Write(" ");
             }
 
 
+
             for (int X = 0; X < quantidadeDeX; X++)
             {
-                Console.Write(estilo);
+                Console.Write(alphabetoArray[contador]);
             }
+
+
         }
     }
 }
